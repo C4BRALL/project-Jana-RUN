5ed1c6460a22f59efc6c66bbcf24da60f2a18b60
Cabral <jcabral304@gmail.com>
2022-03-24 00:51:40 -0300

    react app has rewrite

--- jana-run-app/src/App.js
+++ jana-run-app/src/App.js
@@ -1,25 +1,10 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navbar from "./components/navbar/Navbar";
-import Login from "./pages/Login";
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
-import WppGroup from "./pages/WppGroup";
-import Contact from "./pages/Contact";
-import Register from "./pages/Register";
+import React from 'react';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" exact element={ <Home /> }></Route>
-        <Route path="/wpp" element={ <WppGroup /> }></Route>
-        <Route path="/signin" element={ <Login /> }></Route>
-        <Route path="/register" element={ <Register /> }></Route>
-        <Route path="/contact" element={ <Contact /> }></Route>
-        <Route path="*" element={ <NotFound /> }></Route>
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <h1>hello</h1>
+    </div>
   );
 }
 
